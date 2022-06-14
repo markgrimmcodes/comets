8779a6c741ec19083bfacda8d0defeb99f59506e
Mark Grimm <mark.grimm.coding@gmail.com>
2022-06-14 14:41:03 +0200

    comet in your face

--- sketch.js
+++ sketch.js
@@ -1,8 +1,21 @@
 function setup() {
   createCanvas(400, 400);
   background(0);
+  noStroke();
 }
+let cometX = 0;
+let cometY = 0;
+let cometSize = 0;
 
 function draw() {
-  background(0, 0, 0, 1);
+  background(0, 0, 0, 20);
+  circle(cometX, cometY, (cometSize += 0.5));
+  cometX++;
+  cometY++;
+
+  if (cometX > 400) {
+    cometX = 0;
+    cometY = 0;
+    cometSize = 0;
+  }
 }
